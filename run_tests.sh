8918e135abef98dc0754c2f3a9284d02cc6e692a
Soooda <kongliangxm@foxmail.com>
2019-10-06 06:59:30 +1100

    Update test script
    Divide testcases into two categories: Deterministic and Non-deterministic

--- run_tests.sh
+++ run_tests.sh
@@ -3,11 +3,11 @@
 echo "########################"
 echo "## Testing myfind.py ###"
 echo "########################"
-
+echo
+echo "Non-deterministic testcases: "
 count=0  # number of test cases run so far
-
-for t in test/*.in; do
-	name=$(basename $t .in)
+for t in test/*.sortin; do
+	name=$(basename $t .sortin)
 	output=test/$name.out
     
     text=$(cat $t)
@@ -22,6 +22,30 @@ for t in test/*.in; do
 	count=$((count + 1))
     echo -------------------------------------------------------
 done
+echo "Finished running $count tests!"
+
+echo
+echo
+echo
+
+echo "Deterministic testcases: "
+count=0  # number of test cases run so far
+for t in test/*.in; do
+	name=$(basename $t .in)
+	output=test/$name.out
+    
+    text=$(cat $t)
+    readarray args <<< "$text"
+    # echo ${args[*]}
+    
+	if ./myfind.py ${args[*]} | diff - $output ; then
+        echo "Test $name: Succeeded"
+    else
+        echo "Test $name: Failed"
+    fi
+	count=$((count + 1))
+    echo -------------------------------------------------------
+done
+echo "Finished running $count tests!"
 
 echo "Done!"
-echo "Finished running $count tests!"
