66c287abc48baa98aa40a99558038dbf9115149e
Soooda <kongliangxm@foxmail.com>
2019-10-06 03:29:20 +1100

    Undate test script and Add one testcase

---
+++ run_tests.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh
+
+echo "########################"
+echo "## Testing myfind.py ###"
+echo "########################"
+
+count=0  # number of test cases run so far
+
+for t in test/*.in; do
+	name=$(basename $t .in)
+	output=test/$name.out
+    
+    text=$(cat $t)
+    readarray -t args <<< "$text"
+    # echo ${args[*]}
+    
+	if ./myfind.py ${args[*]} | sort - | diff - $output ; then
+        echo "Test $name: Succeeded"
+    else
+        echo "Test $name: Failed"
+    fi
+	count=$((count + 1))
+    echo 
+done
+
+echo "Done!"
+echo "Finished running $count tests!"
