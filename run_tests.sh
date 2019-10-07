ac5902f0de7e4f04415d4e5e56e18e9680e62eee
Soooda <kongliangxm@foxmail.com>
2019-10-07 17:24:02 +1100

    Update run_tests.sh
    Divide testcases into three categories

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,31 @@ echo "########################"
 echo "## Testing myfind.py ###"
 echo "########################"
 echo
+
+echo "Deterministic testcases: "
+count=0  # number of test cases run so far
+for t in test/*.in; do
+	name=$(basename $t .in)
+	output=test/$name.out
+    
+    text=$(cat $t)
+    readarray args <<< "$text"
+    # echo ${args[*]}
+    
+	if ./myfind.py ${args[*]} | diff - $output ; then
+        echo "Test $name: Succeeded"
+    else
+        echo "Test $name: Failed"
+    fi
+	count=$((count + 1))
+    echo -------------------------------------------------------
+done
+echo "Finished running $count tests!"
+
+echo
+echo
+echo
+
 echo "Non-deterministic testcases: "
 count=0  # number of test cases run so far
 for t in test/*.sortin; do
@@ -22,24 +47,33 @@ for t in test/*.sortin; do
 	count=$((count + 1))
     echo -------------------------------------------------------
 done
+
 echo "Finished running $count tests!"
 
 echo
 echo
 echo
 
-echo "Deterministic testcases: "
+echo "Exit testcases: "
 count=0  # number of test cases run so far
-for t in test/*.in; do
-	name=$(basename $t .in)
+for t in test/*.errorin; do
+	name=$(basename $t .errorin)
 	output=test/$name.out
     
     text=$(cat $t)
     readarray args <<< "$text"
     # echo ${args[*]}
+    ./myfind.py ${args[*]} 2> test/STDERR
+    rc=$?
     
-	if ./myfind.py ${args[*]} | diff - $output ; then
-        echo "Test $name: Succeeded"
+    
+	if [[ $rc != 0 ]] ; then
+        echo "Nonzero exit code"
+        if diff test/STDERR $output ; then
+            echo "Test $name: Succeeded"
+        else
+            echo "Test $name: Failed"
+        fi
     else
         echo "Test $name: Failed"
     fi
